1b51c80ac5419b05b29a423d5e63e37908973497
Васильев Дмитрий <lotus33497@yandex.ru>
2020-08-31 20:06:22 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package tictactoe;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+
+        char[] matrix = scanner.nextLine().toCharArray();
+        System.out.println("---------");
+        for (int i = 0; i < 9; i += 3) {
+            System.out.println(String.format("| %c %c %c |", matrix[i], matrix[i+1], matrix[i+2]));
+        }
+        System.out.println("---------");
+    }
+}
